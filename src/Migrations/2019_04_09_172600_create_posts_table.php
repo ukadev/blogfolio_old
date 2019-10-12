c4637285845ecc80d6c74cbf667014f588b1a06b
ukadev <ukadev@users.noreply.github.com>
2019-10-12 23:31:48 +0200

    Update 2019_04_09_172600_create_posts_table.php

--- src/Migrations/2019_04_09_172600_create_posts_table.php
+++ src/Migrations/2019_04_09_172600_create_posts_table.php
@@ -18,13 +18,13 @@ class CreatePostsTable extends Migration
 		Schema::create($this->tablename, function(Blueprint $table)
 		{
 			$table->increments('id');
-            $table->unsignedBigInteger('user_id');
-            $table->string('title', 100);
-            $table->string('slug', 100)->unique();
-            $table->text('content');
-            $table->integer('category_id')->unsigned();
-            $table->timestamps();
-        });
+		    	$table->unsignedBigInteger('user_id');
+		    	$table->string('title', 100);
+		    	$table->string('slug', 100)->unique();
+		    	$table->text('content');
+		    	$table->integer('category_id')->unsigned();
+		    	$table->timestamps();
+		});
 
         Schema::table($this->tablename, function(Blueprint $table) {
             $table->foreign('user_id')->references('id')->on('users')->onDelete('CASCADE');
