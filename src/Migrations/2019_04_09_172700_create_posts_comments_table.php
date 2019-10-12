fb37b422c3b7a4d6eb0288500ad1e605cb3efb4d
ukadev <ukadev@users.noreply.github.com>
2019-10-12 23:37:01 +0200

    Update 2019_04_09_172700_create_posts_comments_table.php

--- src/Migrations/2019_04_09_172700_create_posts_comments_table.php
+++ src/Migrations/2019_04_09_172700_create_posts_comments_table.php
@@ -18,15 +18,15 @@ class CreatePostsCommentsTable extends Migration
 		Schema::create($this->tablename, function(Blueprint $table)
 		{
 			$table->increments('id');
-            $table->text('content');
-            $table->integer('user_id')->unsigned();
-            $table->integer('post_id')->unsigned();
-            $table->integer('status');
-            $table->timestamps();
+			$table->text('content');
+			$table->unsignedBigInteger('user_id');
+			$table->integer('post_id')->unsigned();
+			$table->integer('status');
+			$table->timestamps();
 
-            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
-            $table->foreign('post_id')->references('id')->on('posts')->onDelete('cascade');
-        });
+			$table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+			$table->foreign('post_id')->references('id')->on('posts')->onDelete('cascade');
+		});
 	}
 
 	/**
